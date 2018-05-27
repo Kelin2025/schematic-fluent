61bcea691e7139c3e73deabf52b3c0815fe4bad6
Anton Kosykh <kelin2025@yandex.ru>
2018-05-27 15:30:13 +0300

    Move ctx to last

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const fluent = ({ methods, executors, defaults }) => () => {
 
   for (const [method, cb] of Object.entries(methods)) {
     res[method] = (...args) => {
-      Object.assign(ctx, cb(ctx, ...args))
+      Object.assign(ctx, cb(...args, ctx))
       return res
     }
   }
 
   for (const [method, cb] of Object.entries(executors)) {
-    res[method] = (...args) => cb(ctx, ...args)
+    res[method] = (...args) => cb(...args, ctx)
   }
 
   return res
