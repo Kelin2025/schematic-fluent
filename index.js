443498aedd892e974e9d5ff36ff2f60b95f4adaa
Anton Kosykh <kelin2025@yandex.ru>
2018-05-27 14:33:52 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const fluent = ({ methods, executors, defaults }) => smth => {
+  const ctx = defaults ? defaults() : {}
+  const res = {}
+
+  for (const [method, cb] of Object.entries(methods)) {
+    res[method] = (...args) => {
+      Object.assign(ctx, cb(ctx, ...args))
+      return res
+    }
+  }
+
+  for (const [method, cb] of Object.entries(executors)) {
+    res[method] = (...args) => cb(ctx, ...args)
+  }
+
+  return res
+}
